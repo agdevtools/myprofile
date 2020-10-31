359a3c9cff8f956744ce94db4fff24a22082b585
Bronagh Gaillard <bronaghgaillard@bronaghs-air.home>
2020-10-31 20:31:25 +0000

    create repo

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -15,7 +15,7 @@ export default class Introduction extends Component {
                       <div className="slider-text-inner js-fullheight">
                         <div className="desc">
                           <h1>Hi! <br />I'm Armand. </h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://docs.google.com/document/" target="_blank" rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>
+                          <p><a className="btn btn-primary btn-learn" href="https://docs.google.com/document" target="_blank" rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>
                         </div>
                       </div>
                     </div>
