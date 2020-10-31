20b25ac429632d3fcf4aae5fef593e6191173887
Bronagh Gaillard <bronaghgaillard@bronaghs-air.home>
2020-10-31 19:59:18 +0000

    first build

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -11,10 +11,10 @@ export default class About extends Component {
                 <div className="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
                 <div className="col-md-12">
                     <div className="about-desc">
-                    <span className="heading-meta">About Us</span>
+                    <span className="heading-meta">About Me</span>
                     <h2 className="colorlib-heading">Who Am I?</h2>
-                    <p>I am a computer grad student from DAIICT-Gandhinagar with ICT as my major.I love exploring new technologies and often amazed by the progress we as a human species have mad so far in the recent years (apart from the headphone-jack part!!)</p>
-                    <p>I have started reflecting my ideas and thougths through the medium of words recently so spelling and grammer mistaks are very often.You can write me back if you spot any and don't want to live anymore :P </p>
+                    <p>I am an experienced software engineer with over 18 years in the industry working across many technologies. I enjoy creating new apps and solving problems.</p>
+                    <p>In recent years i have been honing my skills as a full stack developer in Arity.</p>
                     </div>
                 </div>
                 </div>
@@ -26,8 +26,8 @@ export default class About extends Component {
         <div className="colorlib-narrow-content">
             <div className="row">
             <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
-                <span className="heading-meta">What I do?</span>
-                <h2 className="colorlib-heading">Here are some of my expertise</h2>
+                <span className="heading-meta">What do I do?</span>
+                <h2 className="colorlib-heading">Here are some of my recent experiences</h2>
             </div>
             </div>
             <div className="row row-pt-md">
@@ -37,8 +37,8 @@ export default class About extends Component {
                     <i className="icon-bulb" />
                 </span>
                 <div className="desc">
-                    <h3>Web Development </h3>
-                    <p>I have experience building websites and chrome extentions using JavaScript,React,HTML,CSS</p>
+                    <h3>Front end Development Using React</h3>
+                    <p>I have some recent experience building web apps using react and wiring them to backend services using nginx.</p>
                 </div>
                 </div>
             </div>
@@ -48,8 +48,8 @@ export default class About extends Component {
                     <i className="icon-phone3" />
                 </span>
                 <div className="desc">
-                    <h3>Data Structures & Algorithms</h3>
-                    <p>As coming from the CS background, I have good grasp over fundamental concepts of DSA</p>
+                    <h3>Java, Kotlin and Spring</h3>
+                    <p>I have extensive experience creating back-end services using Spring Boot with Java and Kotlin.</p>
                 </div>
                 </div>
             </div>
@@ -59,8 +59,8 @@ export default class About extends Component {
                     <i className="icon-data" />
                 </span>
                 <div className="desc">
-                    <h3>Dev Ops</h3>
-                    <p>I am pursuing my internship with DevOps team at Juniper and working with tools like Jenkins, Docker, K8s</p>
+                    <h3>Databases and Data Engineering</h3>
+                    <p>Decades of experience using SQL, ETL and reporting tools with a variety of databases, e.g. Cassandra, Oracle and Postgres. I have also experience using pandas and pyspark to extract and cleanse data.</p>
                 </div>
                 </div>
             </div>
